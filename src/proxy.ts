8d4b8aef4e86df2c66ea8e1b781277b06d257364
Ján Timoranský <15653065+timoransky@users.noreply.github.com>
2026-05-11 16:20:55 +0200

    chore: middleware -> proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { supabaseResponse, user } = await updateSession(request);
 
   const { pathname } = request.nextUrl;
